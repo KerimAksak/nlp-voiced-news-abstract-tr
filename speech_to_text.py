6d627d0a9bf569c3aba213cf622176ff1457b9a5
KerimAksak <abdulkerim1783@hotmail.com>
2020-07-03 09:53:01 -0400

    ses dosyasını csv içine yazma denemesi

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,11 +1,16 @@
 import speech_recognition as sr
+import csv
+
+haber_list = open('haber_list.csv','w')
+writer = csv.writer(haber_list)
 
 r = sr.Recognizer()
-file = sr.AudioFile('sound/ses_dosyasi_1_wav.wav')
+file = sr.AudioFile('sound/ses_dosyasi_1 _besiktas.wav')
 
 with file as source:
     r.adjust_for_ambient_noise(source)
     audio = r.record(source)
     result = r.recognize_google(audio, language='tr')
 
+writer.writerows(result)
 print(result)
